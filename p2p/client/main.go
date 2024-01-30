db6e064125d3c901efe6dc4b4627d72196072c73
anhongkui <anhongkui@jd.com>
2024-01-30 12:07:20 +0800

    update

---
+++ p2p/client/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var server string
+
+func runAsClient() {
+	local := &net.UDPAddr{IP: net.IPv4zero, Port: 65001}
+	dest := &net.UDPAddr{IP: net.ParseIP(server), Port: 65002}
+
+	conn, err := net.DialUDP("udp", local, dest)
+	check(err)
+
+	_, err = conn.Write([]byte("hello world."))
+	check(err)
+
+	var data = make([]byte, 8192)
+	n, _, err := conn.ReadFromUDP(data)
+	check(err)
+
+	var peer net.UDPAddr
+	binary.Read(bytes.NewReader(data[:n]), binary.BigEndian, &peer)
+
+	conn, err = net.DialUDP("udp", local, &peer)
+	check(err)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			_, err := conn.Write([]byte(fmt.Sprintf("-- %d -- \n", i)))
+			check(err)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for {
+		n, r, err := conn.ReadFromUDP(data)
+		check(err)
+
+		fmt.Printf("read from %v: %v\n", r.String(), string(data[:n]))
+	}
+
+}
+
+var rootCmd = cobra.Command{
+	Use: "",
+	Run: func(cmd *cobra.Command, args []string) {
+		runAsClient()
+	},
+}
+
+func main() {
+	rootCmd.PersistentFlags().StringVarP(&server, "server", "s", "127.0.0.1", "server address")
+	rootCmd.Execute()
+}
+
+func check(e any) {
+	if e != nil {
+		panic(e)
+	}
+}
---
+++ p2p/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 65002})
+	check(err)
+
+	var peers = make([]*net.UDPAddr, 0, 2)
+	var data = make([]byte, 8192)
+
+	for {
+		n, remote, err := listener.ReadFromUDP(data)
+		check(err)
+
+		fmt.Printf("read from %v: %v\n", remote.String(), string(data[:n]))
+		peers = append(peers, remote)
+
+		if len(peers) == 2 {
+
+			r1 := bytes.NewBuffer(data)
+			binary.Write(r1, binary.BigEndian, peers[0])
+			listener.WriteToUDP(r1.Bytes(), peers[1])
+
+			r2 := bytes.NewBuffer(data)
+			binary.Write(r2, binary.BigEndian, peers[1])
+			listener.WriteToUDP(r2.Bytes(), peers[0])
+			break
+		}
+	}
+}
+
+func check(e any) {
+	if e != nil {
+		panic(e)
+	}
+}
